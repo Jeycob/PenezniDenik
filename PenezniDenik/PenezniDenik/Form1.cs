6dec1e97cc398054559bb4e796c7908bc6cfaf2d
HooDr <jakub.kremer.mail@gmail.com>
2014-07-12 11:40:24 +0200

    PdfSharp helloword

--- PenezniDenik/PenezniDenik/Form1.cs
+++ PenezniDenik/PenezniDenik/Form1.cs
@@ -7,7 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Diagnostics;
+using System.IO;
+using System.Drawing;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
 
 namespace PenezniDenik
 {
@@ -21,6 +27,40 @@ namespace PenezniDenik
         private void button1_Click(object sender, EventArgs e)
         {
             Database db = new Database();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
+
+            // Create an empty page
+            PdfPage page = document.AddPage();
+
+            // Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            //XPdfFontOptions options = new XPdfFontOptions(PdfFontEncoding.Unicode, PdfFontEmbedding.Always);
+
+            // Create a font
+            XFont font = new XFont("Times New Roman", 20, XFontStyle.BoldItalic);
+
+            // Draw the text
+            gfx.DrawString("Hello, World!", font, XBrushes.Black,
+              new XRect(0, 0, page.Width, page.Height),
+              XStringFormats.Center);
+
+            // Save the document...
+            const string filename = "HelloWorld_tempfile.pdf";
+            document.Save(filename);
+            // ...and start a viewer.
+            Process.Start(filename);
         }
     }
 }
